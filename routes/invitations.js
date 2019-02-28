89fa97839db80c6f547ffa40dd63b07776f4652c
allan.ramirez <allan.ramirez@e2outlook.com>
2019-02-28 11:30:35 -0600

    Added invite endpoint

---
+++ routes/invitations.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+var invitationModel = require("../models/invitations/invitations");
+
+router.post("/invite", function(req, res, next) {
+    invitationModel // call the promise
+        .invite(req)
+        .then(
+            function(response) { //success
+                console.log("Success!");
+                res.send(response); //return the data
+            },
+            function(error) { //failed
+                console.error("Failed!", error);
+                res.status(404).send(error); //return error with 404
+            }
+        )
+        .catch(function(ex) { //exception
+            console.error("Exception!", ex);
+            res.status(500).send(ex); //return exception with 500
+        });
+});
+
+module.exports = router;
