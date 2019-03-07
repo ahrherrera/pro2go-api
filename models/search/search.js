674804bae7ae7011edc62e7bb16aa0d264df1bad
allan.ramirez <allan.ramirez@e2outlook.com>
2019-03-07 10:28:50 -0600

    Added getContractorProfile Endpoint

--- models/search/search.js
+++ models/search/search.js
@@ -57,6 +57,62 @@ exports.search = function(req) {
             data.msg.Message = "Unauthorized";
             return reject(data);
         }
-    })
+    });
+
+}
+
+exports.getContractorProfile = function(req) {
+    return new Promise((resolve, reject) => {
+        var data = {};
+        data.msg = { Code: 200, Message: 'Exito!', Tipo: 'n/a' };
+        var conn = config.findConfig();
+
+        const bearerHeader = req.headers['authorization'];
+        if (typeof bearerHeader !== 'undefined') {
+            const bearer = bearerHeader.split(' ');
+            const bearerToken = bearer[1];
+            req.token = bearerToken;
+            jwt.verify(req.token, 'Y2Ae7kXZ', (err, authData) => {
+                if (err) {
+                    data.msg.Code = 400;
+                    data.msg.Message = "Unauthorized";
+                    return reject(data);
+                } else {
+                    sql.connect(conn).then(function() {
+                        var request = new sql.Request();
+                        request.input('ProfileID', sql.Int, req.body.ProfileID);
+
+                        request.execute("[dbo].sp_getContractorProfile").then(function(recordsets) {
+                            let rows = recordsets.recordset;
+                            var mainKey = rows[0];
+                            var selectedKey;
+                            for (var key in mainKey) {
+                                selectedKey = key;
+                            }
+                            sql.close();
+                            return resolve(mainKey[selectedKey]);
+                        }).catch(function(err) {
+                            data.msg.Code = 500;
+                            //TODO: EN produccion cambiar mensajes a "Opps! Something ocurred."
+                            data.msg.Message = err.message;
+
+                            sql.close();
+                            return reject(data);
+                        });
+                    }).catch(function(err) {
+                        data.msg.Code = 500;
+                        data.msg.Message = err.message;
+                        sql.close();
+                        return reject(data);
+                    });
+                }
+            });
+        } else {
+            // Unauthorized
+            data.msg.Code = 400;
+            data.msg.Message = "Unauthorized";
+            return reject(data);
+        }
+    });
 
 }
