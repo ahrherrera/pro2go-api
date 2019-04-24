ac8155c3dcd722da630d5752ce229d9011db706f
allan.ramirez <allan.ramirez@e2outlook.com>
2019-04-24 12:30:49 -0600

    finished rating and review

--- models/rating/rating.js
+++ models/rating/rating.js
@@ -24,18 +24,14 @@ exports.rateCustomer = function(req) {
                         var request = new sql.Request(conn);
                         request.input('ContractorID', sql.Int, authData.User.Profile.id);
                         request.input('CustomerID', sql.Int, req.body.customerId);
-                        request.input('rate', sql.Int, req.body.rating);
+                        request.input('rate', sql.Int, req.body.rate);
                         request.input('comment', sql.VarChar(250), req.body.comment);
 
                         request.execute("dbo.sp_rateCustomer").then(function(recordsets) {
                             let rows = recordsets.recordset;
                             var mainKey = rows[0];
-                            var selectedKey;
-                            for (var key in mainKey) {
-                                selectedKey = key;
-                            }
                             conn.close();
-                            return resolve(mainKey[selectedKey]);
+                            return resolve(mainKey);
                         }).catch(function(err) {
                             data.msg.Code = 500;
                             //TODO: EN produccion cambiar mensajes a "Opps! Something ocurred."
@@ -43,12 +39,14 @@ exports.rateCustomer = function(req) {
                             conn.close();
                             return reject(data);
                         });
+                        console.log(request);
                     }).catch(function(err) {
                         data.msg.Code = 500;
                         data.msg.Message = err.message;
                         conn.close();
                         return reject(data);
                     });
+
                 }
             });
         } else {
@@ -80,11 +78,62 @@ exports.rateContractor = function(req) {
                     conn.connect().then(function() {
                         var request = new sql.Request(conn);
                         request.input('CustomerID', sql.Int, authData.User.Profile.id);
-                        request.input('ContractorID', sql.Int, req.body.contactorId);
-                        request.input('rate', sql.Int, req.body.rating);
+                        request.input('ContractorID', sql.Int, req.body.contractorId);
+                        request.input('rate', sql.Int, req.body.rate);
                         request.input('comment', sql.VarChar(250), req.body.comment);
 
                         request.execute("dbo.sp_rateContractor").then(function(recordsets) {
+                            let rows = recordsets.recordset;
+                            var mainKey = rows[0];
+                            conn.close();
+                            return resolve(mainKey);
+                        }).catch(function(err) {
+                            data.msg.Code = 500;
+                            //TODO: EN produccion cambiar mensajes a "Opps! Something ocurred."
+                            data.msg.Message = err.message;
+                            conn.close();
+                            return reject(data);
+                        });
+                    }).catch(function(err) {
+                        data.msg.Code = 500;
+                        data.msg.Message = err.message;
+                        conn.close();
+                        return reject(data);
+                    });
+                }
+            });
+        } else {
+            // Unauthorized
+            data.msg.Code = 400;
+            data.msg.Message = "Unauthorized";
+            return reject(data);
+        }
+    });
+};
+
+exports.getContractorRating = function(req) {
+    return new Promise((resolve, reject) => {
+        var data = {};
+        data.msg = { Code: 200, Message: 'Exito!', Tipo: 'n/a' };
+        var conn = config.findConfig();
+
+        const bearerHeader = req.headers['authorization'];
+        if (typeof bearerHeader !== 'undefined') {
+            const bearer = bearerHeader.split(' ');
+            const bearerToken = bearer[1];
+            req.token = bearerToken;
+            jwt.verify(req.token, 'Y2Ae7kXZ', (err, authData) => {
+                if (err) {
+                    data.msg.Code = 400;
+                    data.msg.Message = "Unauthorized";
+                    return reject(data);
+                } else {
+                    conn.connect().then(function() {
+                        var request = new sql.Request(conn);
+                        request.input('ContractorID', sql.Int, req.body.contractorId);
+                        request.input('CustomerID', sql.Int, authData.User.Profile.id);
+
+                        request.execute("dbo.sp_getContractorReview").then(function(recordsets) {
                             let rows = recordsets.recordset;
                             var mainKey = rows[0];
                             var selectedKey;
@@ -117,7 +166,7 @@ exports.rateContractor = function(req) {
     });
 };
 
-exports.getRating = function(req) {
+exports.getCustomerReview = function(req) {
     return new Promise((resolve, reject) => {
         var data = {};
         data.msg = { Code: 200, Message: 'Exito!', Tipo: 'n/a' };
@@ -136,10 +185,10 @@ exports.getRating = function(req) {
                 } else {
                     conn.connect().then(function() {
                         var request = new sql.Request(conn);
-                        request.input('ContractorID', sql.Int, req.params.contractorId);
-                        request.input('CustomerID', sql.Int, req.params.customerId);
+                        request.input('ContractorID', sql.Int, authData.User.Profile.id);
+                        request.input('CustomerID', sql.Int, req.body.customerId);
 
-                        request.execute("dbo.sp_getRating").then(function(recordsets) {
+                        request.execute("dbo.sp_getCustomerReview").then(function(recordsets) {
                             let rows = recordsets.recordset;
                             var mainKey = rows[0];
                             var selectedKey;
--- routes/rating.js
+++ routes/rating.js
@@ -41,9 +41,28 @@ router.post("/rateContractor", function(req, res, next) {
         });
 });
 
-router.get("/", function(req, res, next) {
+router.post("/customerRating", function(req, res, next) {
     ratingModel // call the promise
-        .getRating(req)
+        .getCustomerReview(req)
+        .then(
+            function(response) { //success
+                console.log("Success!");
+                res.send(response); //return the data
+            },
+            function(error) { //failed
+                console.error("Failed!", error);
+                res.status(404).send(error); //return error with 404
+            }
+        )
+        .catch(function(ex) { //exception
+            console.error("Exception!", ex);
+            res.status(500).send(ex); //return exception with 500
+        });
+});
+
+router.post("/contractorRating", function(req, res, next) {
+    ratingModel // call the promise
+        .getContractorRating(req)
         .then(
             function(response) { //success
                 console.log("Success!");
